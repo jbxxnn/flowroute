78e877ef98ca7315fdde1a680a28a09c88c955a1
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-05-25 12:32:32 +0100

    fix homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function HomePage() {
           </nav>
         </div>
       </header>
-      <main className="min-h-[calc(100vh-57px-97px)] flex-1">
+      <main className="min-h-[calc(100vh-57px-97px)] grid place-items-center flex-1">
         <div className="container relative pb-10">
           <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-6">
             <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1] mb-5">
