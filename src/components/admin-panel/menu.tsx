71cae23905810b8f5550d259dd53ba6dd3b65285
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-05-27 13:37:47 +0100

    fix sidebar styling

--- src/components/admin-panel/menu.tsx
+++ src/components/admin-panel/menu.tsx
@@ -29,9 +29,9 @@ export function Menu({ isOpen }: MenuProps) {
       <nav className="mt-8 h-full w-full">
         <ul className="flex flex-col min-h-[calc(100vh-48px-36px-16px-32px)] lg:min-h-[calc(100vh-32px-40px-32px)] items-start space-y-1 px-2">
           {menuList.map(({ groupLabel, menus }, index) => (
-            <li className={cn("w-full", groupLabel ? "pt-5" : "")} key={index}>
+            <li className={cn("w-full space-y-2", groupLabel ? "pt-5" : "")} key={index}>
               {(isOpen && groupLabel) || isOpen === undefined ? (
-                <p className="text-sm font-medium text-muted-foreground px-4 pb-2 max-w-[248px] truncate">
+                <p className="text-sm font-medium text-muted-foreground pb-2 max-w-[248px] truncate">
                   {groupLabel}
                 </p>
               ) : !isOpen && isOpen !== undefined && groupLabel ? (
@@ -62,7 +62,7 @@ export function Menu({ isOpen }: MenuProps) {
                               className="w-full justify-start h-10 mb-1"
                               asChild
                             >
-                              <Link href={href}>
+                              <Link href={href} className="flex items-center">
                                 <span
                                   className={cn(isOpen === false ? "" : "mr-4")}
                                 >
