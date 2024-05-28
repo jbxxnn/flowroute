37a69e5831b4a7b36bf7cbe25c11c3e076d085af
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-05-28 11:22:49 +0100

    centralize copyright text

--- src/components/admin-panel/footer.tsx
+++ src/components/admin-panel/footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export function Footer() {
   return (
     <div className="z-20 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="mx-4 md:mx-8 flex h-14 items-center">
-        <p className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
+      <div className="mx-4 md:mx-8 flex h-14 items-center justify-center">
+        <p className="text-xs md:text-sm leading-loose text-muted-foreground">
         Copyright {" "}
             <Link
               href="https://re-circuit.com"
