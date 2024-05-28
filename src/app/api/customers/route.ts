3b22f931e9d5d318de611c17003c3d662d37eebe
jbxxnn <jbxxnn6@gmail.com>
2024-05-28 13:40:23 +0100

    Update

--- src/app/api/customers/route.ts
+++ src/app/api/customers/route.ts
@@ -96,12 +96,12 @@ export async function POST(request: NextRequest) {
     }
 
     const id = "some random id"
-    const {fullname, phone, email, street_address, city, state, zipcode, customer_access_key, customer_secret_key, customer_billing_day, metered_billing_plan, metered_SIP_trunk_usage, cloud_server_hosting_subscription} = _data.data
+    const {fullname, phone, email, street_address, city, state, zip_code, access_key, secret_key, billing_day, metered_sip_trunk_usage, cloud_server_hosting_subscription} = _data.data
 
     // Insert into MySQL
     const [result] = await pool.query(
-      "INSERT INTO customers (fullname, phone, email, street_address, city, state, zipcode, customer_access_key, customer_secret_key, customer_billing_day, metered_billing_plan, metered_SIP_trunk_usage, cloud_server_hosting_subscription) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-      [fullname, phone, email, street_address, city, state, zipcode, customer_access_key, customer_secret_key, customer_billing_day, metered_billing_plan, metered_SIP_trunk_usage, cloud_server_hosting_subscription]
+      "INSERT INTO customers (fullname, phone, email, street_address, city, state, zip_code, access_key, secret_key, customer_billing_day, metered_sip_trunk_usage, cloud_server_hosting_subscription) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [fullname, phone, email, street_address, city, state, zip_code, access_key, secret_key, billing_day, metered_sip_trunk_usage, cloud_server_hosting_subscription]
     ) as [ResultSetHeader, any];
 
     if (result.affectedRows === 1) {
