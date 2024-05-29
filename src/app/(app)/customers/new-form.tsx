090b20aa4fdf1e161ba6319459e904a9dc3ea1c2
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-05-29 14:36:56 +0100

    Extract User Creation Schema

--- src/app/(app)/customers/new-form.tsx
+++ src/app/(app)/customers/new-form.tsx
@@ -18,6 +18,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { addCustomerAPI } from './misc/apis';
 import { useRouter } from 'next/navigation';
 import { fetchPlansQuery } from '../plans/misc/queries';
+import { customerFormSchema } from './misc/zod';
 
 
 const extra_field_options = [
@@ -25,50 +26,6 @@ const extra_field_options = [
   {id: "other", title: "other"},
 ] as const;
 
-const formSchema = z.object({
-  fullname: z.string().min(2, {
-    message: 'Name must be at least 2 characters.',
-  }),
-  phone: z.string().min(2, {
-    message: 'Phone number is required',
-  }).regex(new RegExp(
-    /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
-  ), 'Please enter a valid phone number'),
-  email: z.string().min(1, {
-    message: 'Email must be at least 2 characters.',
-  }).email("Please input a valid email"),
-  street_address: z.string().min(2, {
-    message: 'Street Address must be at least 2 characters.',
-  }),
-  city: z.string().min(2, {
-    message: 'City must be at least 2 characters.',
-  }),
-  state: z.string().min(2, {
-    message: 'State must be at least 2 characters.',
-  }),
-  zip_code: z.string().min(2, {
-    message: 'ZipCode must be at least 2 characters.',
-  }),
-  access_key: z.string().min(2, {
-    message: 'Customer Access Key must be at least 2 characters.',
-  }),
-  secret_key: z.string().min(2, {
-    message: 'Customer Secret Key must be at least 2 characters.',
-  }),
-
-  extra_field: z.string(),
-
-  billing_day: z.number({required_error:"This field is required"}),
-  metered_billing_plan: z.string().min(2, {
-    message: 'Metered Billing Plan must be at least 2 characters.',
-  }),
-  metered_sip_trunk_usage: z.string({required_error:'SIP Trunk usage is required.'}).min(1, {
-    message: 'SIP Trunk usage must be at least 2 characters.',
-  }),
-  cloud_server_hosting_subscription: z.string({required_error: 'Cloud Server Hosting Subscription is required'}).min(1, {
-    message: 'Cloud Server Hosting Subscription must be greater than 0.',
-  }),
-});
 
 interface ProfileFormProps {
   onSubmitted?: () => void;
@@ -94,15 +51,15 @@ export function NewForm({ onSubmitted=()=>{} }: ProfileFormProps) {
     }
   })
   
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof customerFormSchema>>({
+    resolver: zodResolver(customerFormSchema),
     defaultValues: {
       fullname: '',
       extra_field: 'default',
     },
   });
 
-  function handleSubmit(values: z.infer<typeof formSchema>) {
+  function handleSubmit(values: z.infer<typeof customerFormSchema>) {
     addCustomerMutation.mutate(values)
   }
 
--- src/app/api/customers/route.ts
+++ src/app/api/customers/route.ts
@@ -1,6 +1,9 @@
 
 import { NextRequest, NextResponse } from "next/server";
 import mysql from "mysql2/promise";
+import { ResultSetHeader } from "mysql2/promise";
+import { customerFormSchema } from "@/app/(app)/customers/misc/zod";
+import * as z from "zod";
 
 
 // MySQL Connection Configuration (Environment Variables recommended)
@@ -37,61 +40,13 @@ export async function GET(request: NextRequest) {
   }
 }
 
-import * as z from "zod";
-import { ResultSetHeader } from "mysql2/promise";
-
-const formSchema = z.object({
-  fullname: z.string().min(2, {
-    message: 'Name must be at least 2 characters.',
-  }),
-  phone: z.string().min(2, {
-    message: 'Phone number is required',
-  }).regex(new RegExp(
-    /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
-  ), 'Please enter a valid phone number'),
-  email: z.string().min(1, {
-    message: 'Email must be at least 2 characters.',
-  }).email("Please input a valid email"),
-  street_address: z.string().min(2, {
-    message: 'Street Address must be at least 2 characters.',
-  }),
-  city: z.string().min(2, {
-    message: 'City must be at least 2 characters.',
-  }),
-  state: z.string().min(2, {
-    message: 'State must be at least 2 characters.',
-  }),
-  zip_code: z.string().min(2, {
-    message: 'ZipCode must be at least 2 characters.',
-  }),
-  access_key: z.string().min(2, {
-    message: 'Customer Access Key must be at least 2 characters.',
-  }),
-  secret_key: z.string().min(2, {
-    message: 'Customer Secret Key must be at least 2 characters.',
-  }),
-
-  extra_field: z.string(),
-
-  billing_day: z.number({required_error:"This field is required"}),
-  metered_billing_plan: z.string().min(2, {
-    message: 'Metered Billing Plan must be at least 2 characters.',
-  }),
-
-  metered_sip_trunk_usage: z.string({required_error:'SIP Trunk usage is required.'}).min(1, {
-    message: 'SIP Trunk usage must be at least 2 characters.',
-  }),
-  cloud_server_hosting_subscription: z.string({required_error: 'Cloud Server Hosting Subscription is required'}).min(1, {
-    message: 'Cloud Server Hosting Subscription must be greater than 0.',
-  }),
-});
 
 export async function POST(request: NextRequest) {
 
   const data = await request.json();
 
   try {
-    const _data = formSchema.safeParse(data);
+    const _data = customerFormSchema.safeParse(data);
 
 
     if (!_data.success){
