35d61342a8c9a4e52e4ca87dd061b90d02eea1c5
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-06-01 14:13:38 +0100

    update dashboard customer's table

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -13,14 +13,14 @@ export default function Dashboard() {
     <div className="flex min-h-screen w-full flex-col">
      <Navbar title={"Dashboard"} />
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
           <TotalCustomers />
           <ActiveSubscriptions />
           <PricePlans />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
         <CustomerList />
-        <RecentList />
+        {/* <RecentList /> */}
         </div>
       </main>
     </div>
--- src/components/admin-panel/customer-list-dashboard.tsx
+++ src/components/admin-panel/customer-list-dashboard.tsx
@@ -21,147 +21,80 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
+import { fetchCustomersQuery } from "@/app/(app)/customers/misc/queries";
 
-export function CustomerList() {
+function BaseLayout({children}:{children:React.ReactNode}){
   return (
     <Card
-    className="xl:col-span-2" x-chunk="dashboard-01-chunk-4"
-  >
-    <CardHeader className="flex flex-row items-center">
-      <div className="grid gap-2">
-        <CardTitle>Transactions</CardTitle>
-        <CardDescription>
-          Recent transactions from your store.
-        </CardDescription>
-      </div>
-      <Button asChild size="sm" className="ml-auto gap-1">
-        <Link href="#">
-          View All
-          <ArrowUpRight className="h-4 w-4" />
-        </Link>
-      </Button>
-    </CardHeader>
-    <CardContent>
+      className="xl:col-span-2" x-chunk="dashboard-01-chunk-4"
+    >
+      <CardHeader className="flex flex-row items-center">
+        <div className="grid gap-2">
+          <CardTitle>Customers</CardTitle>
+          <CardDescription>
+            These are some of your customers
+          </CardDescription>
+        </div>
+        <Button asChild size="sm" className="ml-auto gap-1">
+          <Link href="/customers">
+            View All
+            <ArrowUpRight className="h-4 w-4" />
+          </Link>
+        </Button>
+      </CardHeader>
+      <CardContent>
+        {children}
+      </CardContent>
+  </Card>
+  )
+
+}
+
+export function CustomerList() {
+
+  const {isLoading, isSuccess, data, isError, error} = fetchCustomersQuery({limit:5})
+
+  if (isLoading){
+    return (
+      <BaseLayout>
+        <p>Loading Customers</p>
+      </BaseLayout>
+    )
+  }
+
+  if (isError){
+    return (
+      <BaseLayout>
+        <p>Error Loading customers</p>
+      </BaseLayout>
+    )
+  }
+
+  return (
+    <BaseLayout>
       <Table>
         <TableHeader>
           <TableRow>
             <TableHead>Customer</TableHead>
-            <TableHead className="hidden xl:table-column">
-              Type
-            </TableHead>
-            <TableHead className="hidden xl:table-column">
-              Status
-            </TableHead>
-            <TableHead className="hidden xl:table-column">
-              Date
-            </TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+            <TableHead className="text-right">Action</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell>
-              <div className="font-medium">Liam Johnson</div>
-              <div className="hidden text-sm text-muted-foreground md:inline">
-                liam@example.com
-              </div>
-            </TableCell>
-            <TableCell className="hidden xl:table-column">
-              Sale
-            </TableCell>
-            <TableCell className="hidden xl:table-column">
-              <Badge className="text-xs" variant="outline">
-                Approved
-              </Badge>
-            </TableCell>
-            <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-              2023-06-23
-            </TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <div className="font-medium">Olivia Smith</div>
-              <div className="hidden text-sm text-muted-foreground md:inline">
-                olivia@example.com
-              </div>
-            </TableCell>
-            <TableCell className="hidden xl:table-column">
-              Refund
-            </TableCell>
-            <TableCell className="hidden xl:table-column">
-              <Badge className="text-xs" variant="outline">
-                Declined
-              </Badge>
-            </TableCell>
-            <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-              2023-06-24
-            </TableCell>
-            <TableCell className="text-right">$150.00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <div className="font-medium">Noah Williams</div>
-              <div className="hidden text-sm text-muted-foreground md:inline">
-                noah@example.com
-              </div>
-            </TableCell>
-            <TableCell className="hidden xl:table-column">
-              Subscription
-            </TableCell>
-            <TableCell className="hidden xl:table-column">
-              <Badge className="text-xs" variant="outline">
-                Approved
-              </Badge>
-            </TableCell>
-            <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-              2023-06-25
-            </TableCell>
-            <TableCell className="text-right">$350.00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <div className="font-medium">Emma Brown</div>
-              <div className="hidden text-sm text-muted-foreground md:inline">
-                emma@example.com
-              </div>
-            </TableCell>
-            <TableCell className="hidden xl:table-column">
-              Sale
-            </TableCell>
-            <TableCell className="hidden xl:table-column">
-              <Badge className="text-xs" variant="outline">
-                Approved
-              </Badge>
-            </TableCell>
-            <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-              2023-06-26
-            </TableCell>
-            <TableCell className="text-right">$450.00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <div className="font-medium">Liam Johnson</div>
-              <div className="hidden text-sm text-muted-foreground md:inline">
-                liam@example.com
-              </div>
-            </TableCell>
-            <TableCell className="hidden xl:table-column">
-              Sale
-            </TableCell>
-            <TableCell className="hidden xl:table-column">
-              <Badge className="text-xs" variant="outline">
-                Approved
-              </Badge>
-            </TableCell>
-            <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-              2023-06-27
-            </TableCell>
-            <TableCell className="text-right">$550.00</TableCell>
-          </TableRow>
+          {data?.map(customer=>(
+            <TableRow key={customer.customer_id}>
+              <TableCell>
+                <div className="font-medium">{customer.fullname}</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  {customer.email}
+                </div>
+              </TableCell>
+              <TableCell className="text-right">
+                <Link href={`/customers/${customer.customer_id}`}>View</Link>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
-    </CardContent>
-  </Card>
+    </BaseLayout>
   );
 }
